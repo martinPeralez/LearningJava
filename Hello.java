b97dd8a88237ae8ac91e5a6f20ef165f3e08fe6a
Martin Peralez <martin.peralez@outlook.com>
2021-05-01 13:23:50 -0500

    init

---
+++ Hello.java
@@ -0,0 +1,32 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+         // C# public static void Main(string[]) 
+         // Almost the same as C#, but in java Main is lower case and string is uppercase
+        System.out.println("Hello World"); 
+        // C# is Console.WriteLine("Hello World"); or System.Console.WriteLine("Hello World");
+        System.out.println("My name is Martin");
+
+        // Comments are the same.
+
+        // Data types
+        //Boolean (true or false), Byte (-128 to 127), Char (characters), short (-32768 to 32767), Int (-2^31 to 2^31-1), Long (-2^63 to 2^62-1), Float, Double, 
+        
+        //Declaration and Initialization are the same Syntax in C#
+        char var1 = 'h'; // C# char var1 = 'h';
+        System.out.println(var1);
+        
+        String var2 = "Hi my name is XYZ."; //C# string var2 = "Hi my name is XYZ."; string is lowercase in C#
+        System.out.println(var2);
+
+        boolean var3 = false; // C# bool var3 = false; in C# boolean is bool
+        System.out.println(var3);
+
+        int var4 = 900; // C# int var4 = 900;
+        System.out.println(var4);
+
+        double var5 = 1.2345; // C# double var5 = 1.2345;
+        System.out.println(var5);
+                
+    }
+}
